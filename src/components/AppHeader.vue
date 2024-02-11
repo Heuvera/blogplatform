3199dec51671d43b980823741c4e081e0b5f39ee
Pascal <pascalokereke18@gmail.com>
2024-02-11 21:36:16 +0100

    blog, posts...individual... router-link.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="container mx-auto flex justify-between text-center">
-        <router-link to ="/" class="text-xl font-bold text-center">My Blog</router-link>
+        <router-link to ="/" class="text-xl font-bold text-center text-blue-900">My Blog</router-link>
          <nav>
             <ul class="flex space-x-4">
-               <li><router-link to="/">Home</router-link></li>
-               <li><router-link to="/about">About</router-link></li>
-               <li><router-link to="/posts">Posts</router-link></li>
+               <li class="text-green-200" tabindex="1"><router-link to="/">Home</router-link></li>
+               <li class="text-green-200" tabindex="2"><router-link to="/posts">Posts</router-link></li>
             </ul>
          </nav>
     </div>
