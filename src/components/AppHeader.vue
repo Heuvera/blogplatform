0b3651239b040933d8eaf36914111867726a1348
Pascal <pascalokereke18@gmail.com>
2024-02-12 11:20:47 +0100

    AppHeader/routes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="container mx-auto flex justify-between text-center">
-        <router-link to ="/" class="text-xl font-bold text-center text-blue-900 py-10 ml-8">My Blog</router-link>
+        <router-link to ="/" class="text-xl font-bold text-center text-blue-900 py-10 ml-8">My Blog
+         <h1 class="text-gray-200 py-6 text-2xl">Hi, I'm Pascal.</h1>
+         </router-link>
          <nav>
-            <ul class="flex space-x-4">
-               <li class="text-green-200 py-6" tabindex="1"><router-link to="/">Home</router-link></li>
-               <li class="text-green-200 py-6" tabindex="2"><router-link to="/posts">Posts</router-link></li>
+            <ul class="flex space-x-4 mr-8 py-8">
+               <li class="text-green-200" tabindex="1"><router-link to="/">Home</router-link></li>
+               <li class="text-green-200" tabindex="2"><router-link to="/posts">Posts</router-link></li>
             </ul>
          </nav>
     </div>
