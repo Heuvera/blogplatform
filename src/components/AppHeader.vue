4ab8f6b5963c5eb1fd07ce45886a4d9bb3e328b5
Pascal <pascalokereke18@gmail.com>
2024-02-15 10:29:32 +0100

    astro

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
     <div class="container mx-auto flex justify-between text-center">
         <router-link to ="/" class="text-xl font-bold text-center text-blue-900 py-10 ml-8">My Blog
          <h1 class="text-gray-200 py-6 text-2xl no-cursor">Hi, I'm Pascal.</h1>
-         <img src="../assets/avatar.jpg" class="w-20 h-auto" />
+         <img src="../assets/avatar.jpg" class="w-20  h-auto" />
          </router-link>
          <nav>
             <ul class="flex space-x-4 mr-8 py-8">
