368d13210b904779da8201a35b39e93197930a0e
Pascal <pascalokereke18@gmail.com>
2024-03-07 00:38:26 +0100

    v

--- src/components/Javascript-blog.vue
+++ src/components/Javascript-blog.vue
@@ -3,6 +3,12 @@
     <div class="flex justify-center mx-auto">
     <div class="text-2xl text-gray-200 font-bold p-2 py-2">
       <router-link to="/null-undefined-blog">Javascript Blog</router-link>
+      <div class="flex flex-col m-10">
+        <ul class="flex-shrink">
+         <li class="text-sm text-gray-200 font-semi">
+          <router-link to="/null-undefined-blog">Null vs Undefined</router-link></li>
+        </ul>
+      </div>
     </div>
     </div>
 </template>
