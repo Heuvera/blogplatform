06670cceff4c0160a92ee366cea7602c2e4b7038
Pascal <pascalokereke18@gmail.com>
2024-03-07 05:11:14 +0100

    r-8

--- src/components/Vue-blog.vue
+++ src/components/Vue-blog.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-center mx-auto">
       <h1 class="font-xl text-gray-200 font-bold">Vue Blog</h1>
       <div class="flex flex-col m-10">
-        <ul class="flex absolute l-8">
+        <ul class="flex absolute r-8">
          <li class="text-sm text-gray-200 font-semi">
           <router-link to="">Multi-word Error in Vue</router-link>
         </li>
