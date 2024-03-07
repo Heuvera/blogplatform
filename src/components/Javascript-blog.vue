260c236e208a5b5ae9ca66fa14e28a6841d3c44f
Pascal <pascalokereke18@gmail.com>
2024-03-07 01:28:50 +0100

    float

---
+++ src/components/Javascript-blog.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="flex justify-center mx-auto">
+      <h1 class="font-xl text-gray-200 font-bold">Javascript Blog</h1>
+      <div class="flex flex-col m-10">
+        <ul class="flex">
+         <li class="text-sm text-gray-200 font-semi float-right">
+          <router-link to="/null-undefined-blog">Null vs Undefined</router-link>
+        </li>
+
+        <br>
+        <li class="text-sm text-gray-200 font-semi float-right">
+          <router-link to="">Keyboard Accessibility</router-link>
+        </li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'javascript-blog',
+}
+</script>
+
+
