dd3a997b996d5b024b38d2ccb919bb1e541d6354
Pascal <pascalokereke18@gmail.com>
2024-03-07 02:53:43 +0100

    menu

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -27,7 +27,7 @@
          <!--Hamburger menu for mobile devices-->
          <div class="md-hidden">
             <button @click="toggleMenu" class="text-blue-900 font-bold py-4">
-               <img src="../assets/hamburger-icon.png" alt="">      
+               <img src="../assets/hamburger-icon.png" class="bg-white" alt="">      
             </button>
 
             <ul v-if="showMenu" class="flex flex-col list-none">
