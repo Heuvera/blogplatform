25aed3ce938779ef457764886818be3bdc5fe094
Pascal <pascalokereke18@gmail.com>
2024-03-07 01:24:48 +0100

    float

--- src/components/Javascript-blog.vue
+++ src/components/Javascript-blog.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-center mx-auto">
       <h1 class="font-xl text-gray-200 font-bold">Javascript Blog</h1>
       <div class="flex flex-col m-10">
-        <ul class="flex absolute tex">
+        <ul class="flex float-right">
          <li class="text-sm text-gray-200 font-semi">
           <router-link to="/null-undefined-blog">Null vs Undefined</router-link>
         </li>
