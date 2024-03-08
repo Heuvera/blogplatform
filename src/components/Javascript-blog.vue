2fdbcde237ad98845a655d33ff8aad0626aba894
Pascal <pascalokereke18@gmail.com>
2024-03-08 07:00:40 +0100

    -space

--- src/components/Javascript-blog.vue
+++ src/components/Javascript-blog.vue
@@ -3,8 +3,8 @@
     <div class="flex flex-col justify-center items-center mx-auto">
       <h1 class="text-3xl text-gray-200 font-bold mb-12">Javascript Blog</h1>
       <div class="flex justify-end">
-        <ul class="flex flex-col list-none mr-64 lg:mr-64">
-         <li class="text-sm text-gray-200 font-semi mb-4 space-x-72">
+        <ul class="flex flex-col list-none mr-64 lg:mr-64 -space-x-80">
+         <li class="text-sm text-gray-200 font-semi mb-4">
           <router-link to="/null-undefined-blog">Null vs Undefined</router-link>
         </li>
 
