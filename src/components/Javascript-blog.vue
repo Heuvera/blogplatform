a4709f2dc360bc6ca0d7336818bfa224c58104b9
Pascal <pascalokereke18@gmail.com>
2024-03-08 05:50:05 +0100

    mr-72

--- src/components/Javascript-blog.vue
+++ src/components/Javascript-blog.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col justify-center items-center relative mx-auto">
       <h1 class="text-3xl text-gray-200 font-bold mb-12">Javascript Blog</h1>
       <div class="flex justify-end">
-        <ul class="flex flex-col list-none mx-14">
+        <ul class="flex flex-col list-none mr-72 ">
          <li class="text-sm text-gray-200 font-semi mb-4">
           <router-link to="/null-undefined-blog">Null vs Undefined</router-link>
         </li>
