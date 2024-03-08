5e8b04ed7bf46ddc4ff842466b77d51d7061dea4
Pascal <pascalokereke18@gmail.com>
2024-03-08 06:45:58 +0100

    mrx

--- src/components/Javascript-blog.vue
+++ src/components/Javascript-blog.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col justify-center items-center mx-auto">
       <h1 class="text-3xl text-gray-200 font-bold mb-12">Javascript Blog</h1>
       <div class="flex justify-end">
-        <ul class="flex flex-col list-none mr-4 md:mr-64">
+        <ul class="flex flex-col list-none mr-64 lg:mr-64">
          <li class="text-sm text-gray-200 font-semi mb-4">
           <router-link to="/null-undefined-blog">Null vs Undefined</router-link>
         </li>
@@ -24,4 +24,4 @@ export default {
 }
 </script>
 
-
+
