b0333ceceb7fb0f1affda4661100eed76c741d0e
Pascal <pascalokereke18@gmail.com>
2024-03-08 06:39:32 +0100

    nulling

--- src/components/Css-tricks.vue
+++ src/components/Css-tricks.vue
@@ -2,8 +2,8 @@
 
   <div class="flex flex-col justify-center items-center relative mx-auto">
     <h1 class="text-3xl text-gray-200 font-bold mb-12">CSS Blog</h1>
-   <div class="flex mr-64">
-    <ul class="flex flex-col list-none mr-64">
+   <div class="flex justify-end">
+    <ul class="flex flex-col list-none mr-4 md:mr-64">
        <li class="text-sm text-gray-200 font-semi mb-4">
         <router-link to="">CSS Animations and Transitions</router-link>
       </li>
