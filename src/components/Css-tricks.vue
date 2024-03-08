9a99529db4c3ceb954bada0ec958b72c1764c47d
Pascal <pascalokereke18@gmail.com>
2024-03-08 06:22:16 +0100

    this mr

---
+++ src/components/Css-tricks.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div class="flex flex-col justify-center items-center relative mx-auto">
+    <h1 class="text-3xl text-gray-200 font-bold mb-12">CSS Blog</h1>
+   <div class="flex mr-96">
+    <ul class="flex flex-col list-none mr-">
+       <li class="text-sm text-gray-200 font-semi mb-4">
+        <router-link to="">CSS Animations and Transitions</router-link>
+      </li>
+
+      <br>
+      <li class="text-sm text-gray-200 font-semi mb-4">
+        <router-link to="">Why i use min-width:0;</router-link>
+      </li>
+      </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CssTricks',
+}
+
+</script>
+
