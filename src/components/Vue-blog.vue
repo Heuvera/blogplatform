11f4613b97ffc42740cd9b15af48a3a8779b2e0e
Pascal <pascalokereke18@gmail.com>
2024-03-08 06:50:23 +0100

    better

--- src/components/Vue-blog.vue
+++ src/components/Vue-blog.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col justify-center relative items-center mx-auto">
       <h1 class="text-3xl text-gray-200 font-bold mb-12">Vue Blog</h1>
       <div class="flex justify-end">
-        <ul class="flex flex-col list-none mr-4 md:mr-64">
+        <ul class="flex flex-col list-none mr-64 md:mr-64">
          <li class="text-sm text-gray-200 font-semi mb-4">
           <router-link to="">Multi-word Error in Vue</router-link>
         </li>
